db2a2e3536dff54f8d20d479b4c3d6a4083acb00
davidaguiler <david.aguilera.galvez@ieselcalamot.com>
2022-10-21 19:23:27 +0200

    Examen David Aguilera (un 10 porfa)

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _HorariState extends State<Horari> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Horari de classe"),
+        title: const Text("Horari de clase"),
       ),
       body: Theme(
         data: Theme.of(context).copyWith(
